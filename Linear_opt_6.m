4a02e7a013be8467c194133dad25b01d5fd598a2
mitch722 <alex.l.mitchell@btinternet.com>
2017-08-29 14:33:04 +0100

    added in the circle script

---
+++ Linear_opt_6.m
@@ -0,0 +1,49 @@
+%% Circle to fit round data
+% Put a circle of the smallest radius which touches at least two points of
+% the sample.
+
+n = 100;
+points =  randn(2,n);
+
+plot(points(1,:),points(2,:),'.')
+
+total = sum(points,2);
+COM = total ./ length(points);
+
+dist_sq = ( COM(1,1) - points(1,:) ).^2 + ( COM(2,1) - points(2,:) ).^2;
+
+[max_dist_sq,index] = max(dist_sq);
+
+dist = sqrt(max_dist_sq);
+
+%% draw a Circle
+
+t = linspace(0,2*pi);
+hold on
+
+axis equal
+plot(dist*cos(t) + COM(1,1), dist*sin(t) + COM(2,1),'r')
+
+%% Move the Circle towards the intersection point
+
+int_sect = points(:,index);
+
+division = ( ( int_sect(2,1) - COM(2,1) ) / (int_sect(1,1) - COM(1,1)) ); 
+
+ang = atan( division );
+
+factor = 1-0.1;
+new_dist = dist*factor;
+
+x_val = new_dist*cos(ang);
+y_val = new_dist*sin(ang); 
+
+if int_sect(1,1) > COM(1,1)
+    x_val = -x_val;
+    y_val = -y_val;
+end
+
+new_c = [int_sect(1,1) + x_val ; int_sect(2,1) + y_val ];
+
+plot(new_dist*cos(t) + new_c(1,1), new_dist*sin(t) + new_c(2,1), 'b' )
+
