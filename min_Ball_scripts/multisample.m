d0935f88d30cf90239cdd52b0f6ad0cb6de28e27
mitch722 <alex.l.mitchell@btinternet.com>
2017-10-09 18:32:10 +0100

    Worked out the pdf for the points

--- min_Ball_scripts/multisample.m
+++ min_Ball_scripts/multisample.m
@@ -5,9 +5,9 @@
 % the sub-sample
 %% Generate the Multisample
 % x.M, no. of global samples in the Multi-sample
-x.M = 10000;
+x.M = 100000;
 % x.no_sub_samp, the no of sets of sub-samples
-x.no_sub_samp = 100;
+x.no_sub_samp = 1000;
 % x.n, no. of samples in the sub-sample
 x.n = x.M / x.no_sub_samp;
 % x.dim, the dimension of the samples
@@ -24,12 +24,22 @@ no_outside = zeros(x.no_sub_samp, 1);
 % cell array containing the sub samples
 Output_data{4, x.no_sub_samp} = [];
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Output data is stored in the rows of a cell array
+sub_samples_entry = 1;
+no_violations_entry = 2;
+violation_points_entry = 3;
+violation_factors_entry = 4;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Run through the sub sample data
+
 tic
 % Runs the for loop in parallel
 for i = 1 : x.no_sub_samp
     % make a cell array for the subsamples
     sub_samp_mat = x.sample( (1 + (i - 1)*x.n ) : i*x.n , : );
-    Output_data{1, i} = sub_samp_mat;
+    Output_data{sub_samples_entry, i} = sub_samp_mat;
     
     [ Rad, cen, no ] = min_R_SOCP(sub_samp_mat);
     
@@ -40,13 +50,41 @@ for i = 1 : x.no_sub_samp
     global_lite = x.sample;
     global_lite( (1 + (i - 1)*x.n ) : i*x.n , : ) = [];
     
-    [ no_violate, output_points, viol_fact ] = violation_function( R, c, global_lite );
+    [ no_violate, output_points, viol_fact ] = violation_function( Rad, cen, global_lite );
     
-    Output_data{2, i} = no_violate;
-    Output_data{3, i} = output_points;
-    Output_data{4, i} = viol_fact;
+    Output_data{no_violations_entry, i} = no_violate;
+    Output_data{violation_points_entry, i} = output_points;
+    Output_data{violation_factors_entry, i} = viol_fact;
 end
 toc    
-        
-    
-    
+
+%% plot epsilon against 
+
+epsilon = linspace(0,0.1,x.no_sub_samp);
+
+e_data = Output_data(violation_factors_entry,:);
+e_data = cell2mat(e_data);
+
+% find how many points are within 0.1 of each value of epsilon
+% ep_repeated = repmat(epsilon, x.no_sub_samp, 1);
+
+diff = e_data' - epsilon;
+diff = abs(diff);
+
+bound = (epsilon(end) - epsilon(1)) / length(epsilon);
+
+logic_number = diff < bound;
+
+freq = sum(logic_number, 1);
+
+figure
+plot(epsilon, freq, '+')
+grid on
+
+pdf = cumsum(freq,2);
+
+pdf = pdf./max(pdf);
+
+figure 
+plot(epsilon, pdf)
+grid on
